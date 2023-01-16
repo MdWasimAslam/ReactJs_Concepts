7a01d0eaced6906ee032343d3ca32d1ece97b775
Md Wasim Aslam <wasimaslam2897@gmail.com>
2023-01-16 21:36:37 +0530

    #

--- src/W3mComponents/Header.js
+++ src/W3mComponents/Header.js
@@ -1,19 +1,15 @@
-import React from 'react'
+import React from "react";
 
-
-const headerStyle={
-        padding: "10px",
-        textAlign: "center",
-        background: "#001461",
-        color: "white",
-        fontSize: "20px",
-}
-
+const headerStyle = {
+  padding: "10px",
+  textAlign: "center",
+  background: "#001461",
+  color: "white",
+  fontSize: "20px",
+};
 
 function Header() {
-    return (
-        <div style={headerStyle}>This is header</div>
-    )
+  return <div style={headerStyle}>This is header</div>;
 }
 
-export default Header
+export default Header;
