efe21957e61755b180953daa7d4bbec3747ed961
Md Wasim Aslam <wasimaslam2897@gmail.com>
2023-01-17 01:33:38 +0530

    #

--- src/W3mComponents/ProjectMovie/Movie.js
+++ src/W3mComponents/ProjectMovie/Movie.js
@@ -3,32 +3,29 @@ import "./Movie.css";
 import movies from "./movie.json";
 import Header from "../Header";
 
-
-
 function Movie() {
   return (
-<>
-<Header/>
-<div className="movieWrapper">
-      {movies.map((value, key) => {
-        return (
-          <>
-            <div class="card">
-              <img src={value.Poster} style={{ width: "100%" }} />
-              <div class="container">
-                <h4>
-                  <b>{value.Title}</b>
-                </h4>
-                <p>Year : {value.Year}</p>
-                <p>IMDB : {value.imdbID}</p>
+    <>
+      <Header />
+      <div className="movieWrapper">
+        {movies.map((value, key) => {
+          return (
+            <>
+              <div className="card">
+                <img src={value.Poster} style={{ width: "100%" }} />
+                <div className="container">
+                  <h4>
+                    <b>{value.Title}</b>
+                  </h4> 
+                  <p>Year : {value.Year}</p>
+                  <p>IMDB : {value.imdbID}</p>
+                </div>
               </div>
-            </div>
-          </>
-        );
-      })}
-    </div>
-</>
-   
+            </>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
