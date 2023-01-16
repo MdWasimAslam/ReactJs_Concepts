3a7db896b35587796a959578c81d861861d5acb8
Md Wasim Aslam <wasimaslam2897@gmail.com>
2023-01-17 00:44:20 +0530

    #

---
+++ src/W3mComponents/ProjectMovie/Movie.js
@@ -0,0 +1,13 @@
+import React from "react";
+function Movie() {
+  return (
+    <div>
+      <div className="movie">
+        <img src="" alt="" />
+        <p>Ironman</p>
+      </div>
+    </div>
+  );
+}
+
+export default Movie;
