bc105cfdb30140bdc791d20c13966c582cbf7898
Md Wasim Aslam <wasimaslam2897@gmail.com>
2023-01-17 01:18:40 +0530

    #

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Jsx from "./W3mComponents/Jsx";
 import Header from "./W3mComponents/Header";
-// import Header from "./Header/Header.js";
-
 import Movie from "./W3mComponents/ProjectMovie/Movie";
 import "./App.css";
 
@@ -10,8 +8,7 @@ import "./App.css";
 function App() {
   return (
     <>
-
-        <Movie />
+    
     </>
   );
 }
