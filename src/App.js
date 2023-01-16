805bfe8ec050990bbbaf53f850dde7b682778538
Wasi <wasimaslam2897@gmail.com>
2023-01-16 20:14:15 +0530

    #

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Jsx from './W3mComponents/Jsx';
-
+import Header from './W3mComponents/Header';
 
 function App() {
   return (
     <>
-    
+    <Header/>
     </>
   )
 }
---
+++ src/W3mComponents/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const headerStyle={
+        padding: "10px",
+        textAlign: "center",
+        background: "#001461",
+        color: "white",
+        fontSize: "20px",
+}
+
+
+function Header() {
+    return (
+        <div style={headerStyle}>This is header</div>
+    )
+}
+
+export default Header
