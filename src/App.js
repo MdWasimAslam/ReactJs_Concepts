750da1c0aba862a668616ca195a427f37def546c
Wasi <wasimaslam2897@gmail.com>
2023-01-17 19:21:42 +0530

    #

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import Jsx from "./W3mComponents/Jsx";
 import Header from "./W3mComponents/Header";
 import Movie from "./W3mComponents/ProjectMovie/Movie";
+import UseStateHook from "./W3mComponents/UseStateHook";
 
 
 function App() {
   return (
     <>
+    <UseStateHook/>
     </>
   );
 }
--- src/W3mComponents/UseStateHook.js
+++ src/W3mComponents/UseStateHook.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Header from './Header'
+import './CSS/UseStateHook.css'
+
+
+
+
 
 function UseStateHook() {
-  return (
-    <div>UseStateHook</div>
-  )
+    const [num, setnum] = useState(0)
+    const incrementNum = () => {
+        setnum(num + 1)
+    }
+
+    const decrementNum = () => {
+        setnum(num > 0 ? num - 1 : 0)
+    }
+
+    const clearNum = () => {
+        setnum(0)
+    }
+
+
+    return (
+        <>
+            <Header />
+            <h1 style={{ textAlign: "center", fontSize: "100px" }}>{num}</h1>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+
+                <button onClick={incrementNum} className={"button"}>Increment</button>
+                <button onClick={decrementNum} className={"button"} id={"decrementBtn"}>Decrement</button>
+                <button onClick={clearNum} className={"button"} id={"clearBtn"}>Clear</button>
+            </div>
+        </>
+    )
 }
 
 export default UseStateHook
