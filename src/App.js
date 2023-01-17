9830e5fc82fd2fb241c0ad8eead237ced2e175fe
Wasi <wasimaslam2897@gmail.com>
2023-01-17 18:43:41 +0530

    #

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
+import "./App.css";
 import Jsx from "./W3mComponents/Jsx";
 import Header from "./W3mComponents/Header";
 import Movie from "./W3mComponents/ProjectMovie/Movie";
-import "./App.css";
 
 
 function App() {
