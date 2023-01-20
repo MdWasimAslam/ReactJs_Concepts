838dcb014e66db4101ab96e1969cc40d18f799bf
Md Wasim Aslam <wasimaslam2897@gmail.com>
2023-01-21 00:42:46 +0530

    #

--- src/W3mComponents/UseEffectHook.js
+++ src/W3mComponents/UseEffectHook.js
@@ -3,10 +3,19 @@ import Header from "./Header";
 
 function UseEffectHook() {
   const [count, setcount] = useState(0);
-  const [countTwo, setcountTwo] = useState(0);
+  const [countTwo, setcountTwo] = useState(1);
+  const [demoData, setdemoData] = useState([]);
 
   useEffect(() => {
-    alert("Rendered!");
+    async function getData() {
+      const data = await fetch(
+        `https://hub.dummyapis.com/employee?noofRecords=${countTwo}&idStarts=1001`
+      );
+      const res = await data.json();
+      setdemoData(res);
+    }
+    getData();
+
     //   }); //When there is no argument it will render everytime
     //   },[]); // When Adding a empty array it will render only once as the dependecies is empty
   }, [countTwo]); // It will render only when the given state is updated
@@ -28,6 +37,17 @@ function UseEffectHook() {
       >
         Click - {countTwo}
       </button>
+      <br></br>
+      {demoData.map((value, index) => {
+        return (
+          <>
+            <p>
+              <b>Name : </b>
+              {value.firstName}
+            </p>{" "}
+          </>
+        );
+      })}
     </>
   );
 }
